fe220a7118337147c8c534043b0fb720df01163f
FishCake <mchaerulhafiz@gmail.com>
2021-10-22 19:14:21 +0700

    first commit

---
+++ src/components/Hero.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="row">
+    <div class="col">
+      <h2>
+
+    <div class=" justify-content-center">
+                   <h2>
+              <strong>Delicious Food Menu, </strong> <br />
+              in Your Gadget
+            </h2>
+
+            <p>Ayo segera pilih dan pesan makanan favorit anda</p>
+            <router-link to="/" class="btn btn-lg btn-success"
+              >Pesan
+            </router-link>
+    
+        </div>
+      </h2>
+    </div>
+    <div class="col">
+      <img src="../assets/images/pacaran.png" width="100%" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Hero",
+};
+</script>
+
+<style>
+</style>
